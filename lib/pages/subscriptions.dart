ca4f95d36a8028bec34bf7574c2b308fadc11836
bralash <emmanuelasaber@gmail.com>
2022-11-13 09:31:07 +0000

    Completed subscriptions UI

--- lib/pages/subscriptions.dart
+++ lib/pages/subscriptions.dart
@@ -17,21 +17,89 @@ class _SubscriptionsState extends State<Subscriptions> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text(FirebaseAuth.instance.currentUser!.displayName!),
-            Text(FirebaseAuth.instance.currentUser!.email!),
-            ElevatedButton(
-              onPressed: () {
-                AuthService().signOut();
-              },
-              child: Text('Log out'),
-            ),
-          ],
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+          child: Column(
+            children: [
+              Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Text(
+                    'Subscriptions',
+                    style: TextStyle(
+                      fontFamily: 'SFPro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 36,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Icon(
+                    Icons.settings_outlined,
+                    size: 24,
+                    color: Colors.grey[600],
+                  ),
+                  SizedBox(width: 15),
+                  Icon(
+                    Icons.add,
+                    size: 24,
+                    color: Colors.blue[900],
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Text(
+                      'Its lonely here',
+                      style: TextStyle(
+                        fontFamily: 'SFPro',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    Text(
+                      'Subscriptions you add will show here',
+                      style: TextStyle(
+                        fontFamily: 'SFPro',
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 22),
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.blue[900],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          'Add subscription',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontFamily: 'SFPro',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
